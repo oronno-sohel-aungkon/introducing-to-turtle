775c6edbc8bcbf304861e613118b115320382ec0
Oronno Sohel Aungkon <oronnosohelaungkon@gmail.com>
2025-08-08 21:05:41 +0600

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,36 @@
+#importing library
+
+import turtle 
+turtle.Screen().bgcolor("Aqua")
+board = turtle.Turtle()
+board.forward(100)
+board.left(120)
+
+board.forward(100)
+
+
+board.left(120)
+
+board.forward(100)
+
+
+board.penup()
+
+board.right(150)
+
+board.forward(50)
+
+
+# second triangle for star
+
+board.pendown()
+
+board.right(90)
+board.forward(100)
+board.right(120)
+board.forward(100)
+board.right(120)
+board.forward(100)
+
+turtle.done()
+
